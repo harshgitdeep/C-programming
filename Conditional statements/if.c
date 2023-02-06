7063685f6fe3c6e5de1c8cbc808592c77accd08e
Harshdeep Singh <88957566+harshgitdeep@users.noreply.github.com>
2023-02-06 23:00:55 +0530

    Create if.c

---
+++ Conditional statements/if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int age;
+    printf("Enter your age\n");
+    scanf("%d",&age);
+
+    if(age>=18){
+        printf("Eligible for voting");
+    }
+
+
+
+    return 0;
+}
