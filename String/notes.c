98332fdb3a14277390b5fddb9c6be1ef16eadfe0
Harshdeep Singh <88957566+harshgitdeep@users.noreply.github.com>
2023-02-22 11:24:46 +0530

    Add files via upload

---
+++ String/notes.c
@@ -0,0 +1,18 @@
+/*
+                    STRING
+charcter type array that is called a string
+
+gets()               scanf()
+puts()               printf()
+
+
+access specifire -----> %s
+header file  ----->       #include<string.h>
+
+-strlwr()
+-strupr()
+-strrev()
+-strlen()
+-stremp()
+etc.
+*/
